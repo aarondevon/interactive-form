4d80b66d24559dfd6b500efd955187f076cb4117
Aaron Sawyer <me@aarondevon.com>
2019-03-21 09:47:07 -0500

    show and hide other job role field

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 // Focus on name field on page load
 $('#name').focus();
+$('#other-title').hide();
+$('#title').change(event => {
+	if ($('#title option:selected').val() === 'other') {
+		$('#other-title').show();	
+	} else {
+		$('#other-title').hide();	
+	}
+})
