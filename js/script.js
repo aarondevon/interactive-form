7f5a7bd8b8acb163b9471a5aa4f44ecd03478350
Aaron Sawyer <me@aarondevon.com>
2019-03-27 08:04:41 -0500

    Display colors for matching design selected

--- js/script.js
+++ js/script.js
@@ -8,3 +8,25 @@ $('#title').change(event => {
 		$('#other-title').hide();	
 	}
 })
+
+$('#design').change(() => {
+	if ($('#design option:selected').val() === 'js puns') {
+		$('#color option:nth-child(1)').attr('selected', true);
+		$('#color option').each((index, value)=> {
+			$(value).show();
+			if (index > 2 && index <= 5) {
+				$(value).hide();
+			}
+		})
+		//$('#other-title').show();	
+	} else if ($('#design option:selected').val() === 'heart js') {
+		$('#color option:nth-child(4)').attr('selected', true);
+		$('#color option').each((index, value)=> {
+			$(value).show();
+			if (index <= 2) {
+				$(value).hide();
+			}
+		}) 
+	}
+})
+
