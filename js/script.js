122a6699962eee0aafe61a2a11cee332eaf8a800
Aaron Sawyer <me@aarondevon.com>
2019-04-02 09:07:57 -0500

    store workshop cost and day

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+let total = 0;
+
 // Focus on name field on page load
 $('#name').focus();
 $('#other-title').hide();
@@ -32,7 +34,17 @@ $('#design').change(() => {
 
 // Disable and enable check boxes
 $('.activities input[type="checkbox"]').on('change', (event) => {
-	
+	// Get cost day, and time
+	const regexCost = /\d{2,}$/;
+	const regexDay = /— \w+ /;
+	cost = parseInt(regexCost.exec($(event.target).parent()[0].textContent)[0]);
+	let day = regexDay.exec($(event.target).parent()[0].textContent);
+	if (!(day === null)) {
+		//day = regexDay.exec($(event.target).parent().textContent)[0];
+		day = day[0];
+ }
+	console.log(cost);
+	console.log(day)
 	// target check boxes for 9am workshops
 	if($(event.target).parent()[0].textContent.includes('Tuesday 9am')) {
 		// disable check boxes
@@ -60,9 +72,12 @@ $('.activities input[type="checkbox"]').on('change', (event) => {
 		// disable check boxes
 		$('.activities input').each((index, element) => {
 			if (!($('.activities input[type="checkbox"]')[index].checked) &&  $(element).parent()[0].textContent.includes('Tuesday 1pm')) {
+				
+
 				$(element).attr('disabled', true);
 				$(element).parent().css('text-decoration', 'line-through');
 				$(element).parent().css('color', '#696969');
+				
 			}
 		})
 		// enable check boxes
@@ -70,11 +85,19 @@ $('.activities input[type="checkbox"]').on('change', (event) => {
 			$('.activities input').each((index, element) => {
 				if($
 					(element).parent()[0].textContent.includes('Tuesday 1pm')) {
+					
+					
 					$(element).attr('disabled', false);
 					$(element).parent().css('text-decoration', 'none');
 				  $(element).parent().css('color', '#000');
 				}
 			})
 		}
-	}	
+	} 
+		
+		// let cost = parseInt($(event.target).parent()[0].textContent.substr(-3));
+		// console.log(cost);
+		// total += cost;
+		console.log(total);
+
 })
